2c1992cc3f7ec946a3d2f063bab30edbba55f748
Argyrios Deligiannidis <a.deligiannidis@gmail.com>
2019-10-15 08:28:39 +0300

    Completed

--- pygrep.py
+++ pygrep.py
@@ -1,36 +1,42 @@
 import os
 import re
 import sys
+import stat
 import argparse
 import tempfile
 import linecache
 
+
 class colorCodes:
+    """This class encapsulates the values for coloring codes to be used when requested """
     RED = '\x1b[1;31;40m'
     PURPLE = '\x1b[35;40m'
     END = '\x1b[0m'
 
 
 class regSearch:
+    """
+    Class that contains all the necessary methods to perform the regex search in the files
+    """
     def __init__(self):
-        #self.argument_handler()
-
         self.args = self.argument_handler()
         self.exp = self.reg_compiler(self.args.regex[0])
-        print('INIT:',self.args)
-        print('INIT:',self.exp)
 
     def reg_compiler(self, regex):
-        exp = re.compile('.*({}).*'.format(regex))
-        return exp
+        try:
+            exp = re.compile('.*({}).*'.format(regex))
+            return exp
+        except Exception as err:
+            print('{}\nRegular expresion {} is invalid'.format(err,regex))
+            exit(2)
 
-    def argument_handler(self, argv=None):
-        """Constructor, initializes the argument parser (argparse) and runs checks on the provided arguments."""
+    def argument_handler(self):
+        """Initializes the argument parser (argparse) and runs checks on the provided arguments."""
         parser = argparse.ArgumentParser(
-            description='Python grep implementation')
+            description='a simple tool that searches files for matches on a regular expression')
         parser.add_argument(
             "-u",
-            "--underscore",
+            "--caret",
             help="Print '^' under the matching text",
             action="store_true")
         parser.add_argument("-c", "--color", help="Colorize matches",
@@ -44,7 +50,7 @@ class regSearch:
             'regex',
             nargs=1,
             action="store",
-            help="Search expresion enclosed in single quotes ex. 'something.*[0-9]'")
+            help="Search expression enclosed in single quotes ex. 'something.*[0-9]'")
         parser.add_argument(
             'files',
             type=argparse.FileType('r'),
@@ -52,15 +58,15 @@ class regSearch:
             default=sys.stdin,
             help="One or two files to be searched for matches of [regex]")
         args = parser.parse_args()
-        #self.exp = re.compile('.*({}).*'.format(args.regex[0]))
-        #for arg in vars(args):
-        #    print(arg, getattr(args, arg), type(getattr(args, arg)))
-        if not (args.underscore or args.machine or args.color):
+
+        if not (args.caret or args.machine or args.color):
             parser.error('Please select the output format [-c, -u, -uc, -m]')
-        if args.machine and (args.underscore or args.color):
+        if args.machine and (args.caret or args.color):
             parser.error('Machine format [-m] cannot be  combined with [-c] or [-u]')
         if len(args.regex[0]) == 0 or args.regex[0].isspace():
             parser.error('Invalid expression') #Error if regex empty or not set
+        if os.path.isfile(args.regex[0]):
+            parser.error('Please type the search regex')
         if sys.stdin.isatty() and args.files is sys.stdin: #Error if no file specified on tty
             parser.error('No input file specified')
         try:
@@ -68,33 +74,37 @@ class regSearch:
                 parser.error('Input files limit of 2 exceeded') #Error if files are more than two
         except TypeError:
             pass
-
         #for arg in vars(args):
         #    print(arg, getattr(args, arg))
-        #print(args)
+
         return args
 
-    def underscore(self, filename, line, line_num, matches):
-        """Inserts the underscore ^ character under every character position (whiteline until then), the start and end position of the match are provided by the [matches] dictionary, the resulting string containing two lines is returned.
+    def caret(self, filename, line, line_num):
+        """
+        Inserts the caret ^ character under every character position
+        (whiteline until then), the start and end position of the match are
+        provided by the [matches] dictionary, the resulting string
+        containing two lines is returned.
         """
         out = '{} line {}: '.format(filename, line_num)
         offset = len(out)
         out = out + '{}\n'.format(line)
-        m_pos = [range(a, b) for a, b in zip( matches['start'], matches['stop'])]
-
-        def flatten(l): return [i + offset for subl in l for i in subl] #Flatten a list of lists
+        match_idx = self.exp.match(line).span(1)
         for c in range(len(line) + offset):
-            if c in flatten(m_pos):
+            if c in range(match_idx[0]+offset,match_idx[1]+offset):
                 out += '^'
             else:
                 out += ' '
         return out
 
     def colorize(self, filename, line, line_num, to_colr):
-        """Returns the input [line] with the matches colorized by partitioning on the matched string [to_colr] and inserting the color codes in the appropriate position.
+        """
+        Returns the input [line] with the matches colorized by partitioning
+        on the matched string [to_colr] and inserting the color codes in the
+        appropriate position.
         """
         out =  colorCodes.PURPLE + '{} line {}:{} '.format(filename, line_num, colorCodes.END)
-        if self.args.underscore:
+        if self.args.caret:
             out = ''
         parts = line.split('{}'.format(to_colr))
         for part in parts[:-1]:
@@ -103,7 +113,8 @@ class regSearch:
         return out
 
     def machine(self, filename, line_num, matches, matched):
-        """Prints out the matches found in every line in machine understood file_name:no_line:start_pos:matched_text ."""
+        """Prints out the matches found in every line in machine
+        understood format file_name:no_line:start_pos:matched_text."""
         out = ''
         out += '{}:{}:{}:{}\n'.format(filename, line_num, matches['start'][0], matched) # Comment line to allow overlap
         #for match in matches['start']: #Comment out to allow overlap
@@ -112,13 +123,19 @@ class regSearch:
         return out
 
     def get_line(self, infile, line_num):
-        """Read the line with line number [line_num] from [infile] and return her."""
+        """
+        Read the line with line number [line_num] from [infile]
+        and returns her.
+        """
         line = linecache.getline(infile.name, line_num)
         line = line.rstrip()
         return line
 
     def match_lines(self, infile):
-        """Generator function used in function match_indices to initialize the matches dictionary"""
+        """
+        Generator function used in function match_indices to initialize the
+        matches dictionary
+        """
         line_num = 0
         for line in infile:
             line_num += 1
@@ -127,7 +144,12 @@ class regSearch:
                 yield line_num
 
     def match_indices(self, infile):
-        """Initializes a dictionary with keys the lines that matched the regular expresion provided, then inserts a sub-dictionary for evrey key that contains the list of positions that matches start and end ex. line: {'start': [start1, start2], 'stop': [stop1, stop2]}"""
+        """
+        Initializes a dictionary with keys the lines that matched the
+        regular expresion provided, then inserts a sub-dictionary for
+        every key that contains the list of positions that matches start and
+        end ex. line: {'start': [start1, start2], 'stop': [stop1, stop2]}
+        """
         matches = {m: None for m in self.match_lines(infile)}
         for match in matches.keys():
             line = self.get_line(infile, match)
@@ -140,8 +162,12 @@ class regSearch:
         return matches
 
     def run(self, infile):
-        """Gets the index for every match, reads the line containing the match from [infile] and calls the appropriate function to format the output as specified by the user and prints it."""
-        #self.args = self.argument_handler()
+        """
+        Gets the index for every match, reads the line containing the match
+        from [infile] and calls the appropriate function to format the output
+        as specified by the user and prints it.
+        """
+        self.ret_code = 0
         res = ''
         matches = self.match_indices(infile)
         for line_num in sorted(matches.keys()):
@@ -155,27 +181,27 @@ class regSearch:
                     line_num,
                     matches[line_num],
                     matched_string)
-            elif self.args.underscore:
-                res = self.underscore(
-                    infile.name, line, line_num, matches[line_num])
+            elif self.args.caret:
+                res = self.caret(
+                    infile.name, line, line_num)
                 if self.args.color:
                     res = self.colorize(
                         infile.name, res, line_num, len(matched_string) * '^')
             elif self.args.color:
                 res = self.colorize(
                     infile.name, line, line_num, matched_string)
-            print(res) # Python 3 compatibility
-
+            sys.stdout.write(res + '\n')
 
 if __name__ == '__main__':
     finder = regSearch()
-    if sys.stdin.isatty():
-        finder.argument_handler()
+    mode = os.fstat(0).st_mode
+    if not stat.S_ISFIFO(mode) and not stat.S_ISREG(mode):
         for file in finder.args.files:
             finder.run(file)
             file.close()  # Close the files handle, because argparse leaves them open
     else:
-        temp = tempfile.NamedTemporaryFile('r+w') #,encoding=ascii' Use tempfile to bypass limitation of re module not being able to use regexes on a stream. Alternatively the regex mdule can be used
+        sys.stdout.write(str(sys.argv))
+        temp = tempfile.NamedTemporaryFile('w+') #,encoding=ascii' Use tempfile to bypass limitation of re module not being able to use regexes on a stream. Alternatively the regex mdule can be used
         try:
             temp.writelines('{}'.format(sys.stdin.read()))
             temp.seek(0)
--- tests/functional/test_arguments_and_functionality.py
+++ tests/functional/test_arguments_and_functionality.py
@@ -1,49 +1,212 @@
 import os
-#import glob
-import subprocess
-#import logging
-#import time
 import sys
 import argparse
-try:
-    from unittest import mock  # python 3.3+
-except ImportError:
-    import mock  # python 2.6-3.2
+import filecmp
 from avocado import Test
 from avocado import main
-#from avocado import mock
+from avocado.utils import process
 
-sys.path.append('../..')
-sys.path.append('./')
-import pygrep
 
-class ArgumentTesting(Test):
+class funcTesting(Test):
+    """
+    Runs the tests, parameters are loaded from the arguments.yaml file, which
+    provides us with 576 variations:
+    :param output_opt1: Output formating option [u,c,m]
+    :param output_opt2: Output formating option [u,c,m]
+    :param regex: Regular expresion used for the tests, two correct, one wrong
+                  and an empty
+    :param file1: First file option for the test [empty, not-provided, file1]
+    :param file2: First file option for the test [empty, not-provided, file2]
+    """
 
-    #os.chdir('../../')
+    def datafiles_generator(self):
+        """
+        Genarates the data files for the possible expected outputs of the
+        regexes for the tests 24 possible outcomes, if they do not exist.
+        """
+        opts = ['-cu', '-c', '-u', '-m']
+        datadir = 'tests/functional/test_arguments_and_functionality.py.data/'
+        test_files = ['{}log_file1.txt'.format(datadir), '{}log_file2.txt'.format(
+            datadir), '{}log_file1.txt {}log_file2.txt'.format(datadir, datadir)]
+        regexes = ['run.*/', 'run']
+        datafiles = [
+            ['cuf1r1.txt', 'cf1r1.txt', 'uf1r1.txt', 'mf1r1.txt'], [
+                'cuf2r1.txt', 'cf2r1.txt', 'uf2r1.txt', 'mf2r1.txt'], [
+                'cuf1f2r1.txt', 'cf1f2r1.txt', 'uf1f2r1.txt', 'mf1f2r1.txt']], [
+            ['cuf1r2.txt', 'cf1r2.txt', 'uf1r2.txt', 'mf1r2.txt'], [
+                'cuf2r2.txt', 'cf2r2.txt', 'uf2r2.txt', 'mf2r2.txt'], [
+                'cuf1f2r2.txt', 'cf1f2r2.txt', 'uf1f2r2.txt', 'mf1f2r2.txt']]
+        for regex, file_super_set in zip(regexes, datafiles):
+            for in_file, file_set in zip(test_files, file_super_set):
+                for opt, out_file in zip(opts, file_set):
+                    if not os.path.isfile(datadir + out_file):
+                        data_file = open(datadir + out_file, "w")
+                        cmd_line = "python pygrep.py {} '{}' {}".format(
+                            opt, regex, in_file)
+                        result = process.run(
+                            cmd_line, ignore_status=True, shell=False)
+                        data_file.write(str(result.stdout.decode("UTF-8")))
+                        data_file.close()
 
-    def test_random_arg(self):
+    def output_comparison(self, expected_file):
+        """
+        Compares the output of the current test with the saved valid output
+        """
+        test_out = self.get_data('test_out.txt')
+        expected_file = self.get_data(expected_file)
+        out_cmp = filecmp.cmp(test_out, expected_file)
+        self.assertTrue(
+            out_cmp,
+            '*** Test file test_out.txt has different output from {}***'.format(expected_file))
+
+    def test_functionality(self):
+        """
+        Runs the tests invoking the script and testing the output. First the
+        expected returns are generated to compansate for testing erroneous
+        parameters. If a test provides output it is stored and compared with
+        previously stored valid outputs
+        """
+        datadir = 'tests/functional/test_arguments_and_functionality.py.data/'
+        empty_file = datadir + 'empty_file.txt'
+        error_try = False
+        ret = None
+        expected_ret = None
+        m_out = False
+        c_out = False
+        u_out = False
         opt1 = self.params.get('output_opt1', '/*')
         opt2 = self.params.get('output_opt2', '/*')
         regex = self.params.get('regex', '/*')
         file1 = self.params.get('file1', '/*')
         file2 = self.params.get('file2', '/*')
-        print('PARAMS=============',opt1, opt2, regex, file1, file2)
-        print(type(regex))
-        #with mock.patch('pygrep.regSearch.argument_handler', return_value=argparse.Namespace(underscore=True,color=True, machine=False, regex=['jedi'], files=[open('../../docs/test_files/file2.txt', 'r')])):
-        #    finder = pygrep.regSearch()
-        #    for file in finder.args.files:
-        #        print("TESTTTTTTTTTTTTTTTTTTTTTTT:",file)
-        #        finder.run(file)
-        #        file.close()
-            #finder.run('../../docs/test_files/file1.txt')
-            #print('RESSSSSSSSSS=========='+finder.args)
-        #data = TestData.get_data(filename='file2.txt')
-        #exit(2)
-        #cmd_line = 'python pygrep.py some-random-text'
-        #ret = subprocess.call(cmd_line, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        #expected_ret = 2
-        #self.assertEqual(ret, expected_ret,
-        #                 'pygrep did not return : %d:\n%s' % (expected_ret, ret))
+
+        if opt1 == 'm' or opt2 == 'm':
+            m_out = True
+        if opt1 == 'c' or opt2 == 'c':
+            c_out = True
+        if opt1 == 'u' or opt2 == 'u':
+            u_out = True
+
+        opt1 = '' if not opt1 else opt1
+        opt2 = '' if not opt2 else opt2
+        file1 = '' if not file1 else datadir + file1
+        file2 = '' if not file2 else datadir + file2
+        regex = '' if not regex else regex
+
+        if not opt1 and not opt2:
+            error_try = True
+            expected_ret = 2
+        elif m_out and (c_out or u_out):
+            error_try = True
+            expected_ret = 2
+        elif not regex or regex == "'*.*con.* '":
+            error_try = True
+            expected_ret = 2
+        elif (not file1 and not file2) and not regex:
+            error_try = True
+            expected_ret = 2
+        elif regex and (not file1 and not file2):
+            # Quick fix because avocado does not see the exit code 2 from
+            # this specific parser.error exception 
+            error_try = True
+            expected_ret = 0
+        elif regex == "'*.*con.* '" and (not file1 and not file2):
+            error_try = True
+            expected_ret = 2
+        elif (file1 == empty_file and file2 == empty_file) and regex:
+            error_try = True  # Skip comparison
+            expected_ret = 0
+        elif file1 == empty_file or file2 == empty_file and (not file1 or not file2) and regex:
+            error_try = True  # Skip comparison
+            expected_ret = 0
+
+        cmd_line = 'python pygrep.py -{}{} {} {} {}'.format(
+            opt1, opt2, regex, file1, file2)
+        if error_try == False:
+            self.datafiles_generator()
+            expected_ret = 0
+            test_output = open(datadir + "test_out.txt", "w")
+            result = process.run(cmd_line, ignore_status=True)
+            test_output.write(str(result.stdout.decode("UTF-8")))
+            test_output.close()
+            ret = result.exit_status
+            self.assertEqual(
+                ret,
+                expected_ret,
+                '*** Return code {} was expected but got {} ***'.format(
+                    expected_ret,
+                    ret))
+
+            file1 = None if file1 == empty_file else file1
+            file2 = None if file2 == empty_file else file2
+
+            if regex == "'run.*/'":
+                if file1 and file2:
+                    if m_out:
+                        self.output_comparison('mf1f2r1.txt')
+                    elif c_out and not u_out:
+                        self.output_comparison('cf1f2r1.txt')
+                    elif u_out and not c_out:
+                        self.output_comparison('uf1f2r1.txt')
+                    elif c_out and u_out:
+                        self.output_comparison('cuf1f2r1.txt')
+                elif file1 and not file2:
+                    if m_out:
+                        self.output_comparison('mf1r1.txt')
+                    elif c_out and not u_out:
+                        self.output_comparison('cf1r1.txt')
+                    elif u_out and not c_out:
+                        self.output_comparison('uf1r1.txt')
+                    elif c_out and u_out:
+                        self.output_comparison('cuf1r1.txt')
+                elif file2 and not file1:
+                    if m_out:
+                        self.output_comparison('mf2r1.txt')
+                    elif c_out and not u_out:
+                        self.output_comparison('cf2r1.txt')
+                    elif u_out and not c_out:
+                        self.output_comparison('uf2r1.txt')
+                    elif c_out and u_out:
+                        self.output_comparison('cuf2r1.txt')
+            else:
+                if file1 and file2:
+                    if m_out:
+                        self.output_comparison('mf1f2r2.txt')
+                    elif c_out and not u_out:
+                        self.output_comparison('cf1f2r2.txt')
+                    elif u_out and not c_out:
+                        self.output_comparison('uf1f2r2.txt')
+                    elif c_out and u_out:
+                        self.output_comparison('cuf1f2r2.txt')
+                elif file1 and not file2:
+                    if m_out:
+                        self.output_comparison('mf1r2.txt')
+                    elif c_out and not u_out:
+                        self.output_comparison('cf1r2.txt')
+                    elif u_out and not c_out:
+                        self.output_comparison('uf1r2.txt')
+                    elif c_out and u_out:
+                        self.output_comparison('cuf1r2.txt')
+                elif file2 and not file1:
+                    if m_out:
+                        self.output_comparison('mf2r2.txt')
+                    elif c_out and not u_out:
+                        self.output_comparison('cf2r2.txt')
+                    elif u_out and not c_out:
+                        self.output_comparison('uf2r2.txt')
+                    elif c_out and u_out:
+                        self.output_comparison('cuf2r2.txt')
+
+        else:
+            result = process.run(cmd_line, ignore_status=True)
+            ret = result.exit_status
+            self.assertEqual(
+                ret,
+                expected_ret,
+                '*** Return code {} was expected but got {} ***'.format(
+                    expected_ret,
+                    ret))
+
 
 if __name__ == "__main__":
     main()
